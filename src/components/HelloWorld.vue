ae8b8063029e8a919d0dc498d5f8b701a8abce63
bchuup <bchuup@gmail.com>
2018-03-18 17:00:59 -0400

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="">
+    <h1>
+      list
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="styl" scoped>
+  @import '~styl/colors.styl'
+
+  h1, h2 {
+    font-weight: normal;
+    color: $green;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
